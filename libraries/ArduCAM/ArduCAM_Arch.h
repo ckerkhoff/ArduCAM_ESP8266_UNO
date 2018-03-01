e77ec34fb4d95b3d51ad800b25fa9c32b2dbddc7
Chris Kerkhoff <christopher.kerkhoff@gmail.com>
2018-02-28 19:09:51 -0600

    Moving platform specific defines

--- libraries/ArduCAM/ArduCAM_Arch.h
+++ libraries/ArduCAM/ArduCAM_Arch.h
@@ -5,6 +5,91 @@
  #include "Arduino.h"
 #endif
 
+#if defined (__AVR__)
+    #define cbi(reg, bitmask) *reg &= ~bitmask
+    #define sbi(reg, bitmask) *reg |= bitmask
+    #define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+    #define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+    #define cport(port, data) port &= data
+    #define sport(port, data) port |= data
+    #define swap(type, i, j) {type t = i; i = j; j = t;}
+    #define fontbyte(x) pgm_read_byte(&cfont.font[x])
+    #define regtype volatile uint8_t
+    #define regsize uint8_t
+#endif
+
+#if defined(__SAM3X8E__)
+    #define cbi(reg, bitmask) *reg &= ~bitmask
+    #define sbi(reg, bitmask) *reg |= bitmask
+    #define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+    #define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+    #define cport(port, data) port &= data
+    #define sport(port, data) port |= data
+    #define swap(type, i, j) {type t = i; i = j; j = t;}
+    #define fontbyte(x) cfont.font[x]
+    #define regtype volatile uint32_t
+    #define regsize uint32_t
+    #define PROGMEM
+    #define pgm_read_byte(x)        (*((char *)x))
+    #define pgm_read_word(x)        ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x)))
+    #define pgm_read_byte_near(x)   (*((char *)x))
+    #define pgm_read_byte_far(x)    (*((char *)x))
+    #define pgm_read_word_near(x)   ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x)))
+    #define pgm_read_word_far(x)    ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x))))
+    #define PSTR(x)  x
+    #if defined F
+	    #undef F
+    #endif
+    #define F(X) (X)
+#endif
+
+#if defined(ESP8266)
+	#define cbi(reg, bitmask) digitalWrite(bitmask, LOW)
+	#define sbi(reg, bitmask) digitalWrite(bitmask, HIGH)
+	#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+	#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+	#define cport(port, data) port &= data
+	#define sport(port, data) port |= data
+	#define swap(type, i, j) {type t = i; i = j; j = t;}
+	#define fontbyte(x) cfont.font[x]
+	#define regtype volatile uint32_t
+	#define regsize uint32_t
+#endif
+
+#if defined(__CPU_ARC__)
+	#define cbi(reg, bitmask) *reg &= ~bitmask
+	#define sbi(reg, bitmask) *reg |= bitmask
+	#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+	#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+	#define cport(port, data) port &= data
+	#define sport(port, data) port |= data
+	#define swap(type, i, j) {type t = i; i = j; j = t;}
+	#define fontbyte(x) pgm_read_byte(&cfont.font[x])
+	#define regtype volatile uint32_t
+	#define regsize uint32_t
+#endif
+
+#if defined (RASPBERRY_PI)
+	#define regtype volatile uint32_t
+	#define regsize uint32_t
+	#define byte uint8_t
+	#define cbi(reg, bitmask) digitalWrite(bitmask, LOW)
+    #define sbi(reg, bitmask) digitalWrite(bitmask, HIGH)
+    #define PROGMEM
+	//#define pgm_read_byte(x)        (*((char *)x))
+	//#define pgm_read_word(x)        ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x)))
+	//#define pgm_read_byte_near(x)   (*((char *)x))
+	//#define pgm_read_byte_far(x)    (*((char *)x))
+	//#define pgm_read_word_near(x)   ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x)))
+	//#define pgm_read_word_far(x)    ( ((*((unsigned char *)x + 1)) << 8) + (*((unsigned char *)x))))
+
+	#define PSTR(x)  x
+	#if defined F
+	    #undef F
+	#endif
+	#define F(X) (X)
+#endif
+
 class ArduCAM_Arch
 {
   public:
